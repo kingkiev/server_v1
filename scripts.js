cdc515ab64d5d9351cdf5f7536d7abc03f3d7c78
kingkiev <49226820+kingkiev@users.noreply.github.com>
2019-04-11 22:21:29 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+advise_url = "http://sf-pyw.mosyag.in/m04/api/forecasts"
+
+$("#main-header-1").click(function() {
+
+	$.getJSON(advise_url, function(data) {
+			set_content_in_divs(data['prophecies'])
+	
+	});		
+});
+
+function set_content_in_divs(paragraphs) {
+  $.each(paragraphs, function(i, d) {
+    p = $("#p-" + i)
+    p.html("<p>" + d + "</p>")
+  })
+}
+
